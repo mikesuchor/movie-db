166480c1b8777853c5b3fd0f3aeb328a230c6915
mikesuchor <mikesuchor@gmail.com>
2020-06-12 18:18:50 -0400

    Refactored get functions

--- src/components/App.js
+++ src/components/App.js
@@ -2,45 +2,67 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import FeaturedMovie from "./FeaturedMovie";
 import MoviesList from "./MoviesList";
+import Footer from "./Footer";
 import tmdb from "../api/tmdb";
 import "./css/App.css";
 
 class App extends React.Component {
-  state = { movies: [], featuredMovie: "" };
+  state = {
+    dataLoaded: false,
+    movies: [],
+    featuredMovie: "",
+    featuredMovieTrailer: ""
+  };
 
-  async componentDidMount() {
-    const response = await tmdb.get("/3/discover/movie/", {
-      params: {
-        api_key: "1155f6c239cb4332df695fcf245eaffd"
-      }
-    });
-    this.setState({
-      movies: response.data.results,
-      featuredMovie: response.data.results[0]
-    });
-  }
-
-  onFormSubmit = async (input) => {
-    const response = await tmdb.get("/3/search/movie/", {
+  getMovies = async (action, query = "") => {
+    const movies = await tmdb.get(`/3/${action}/movie/`, {
       params: {
         api_key: "1155f6c239cb4332df695fcf245eaffd",
-        query: input
+        query
       }
     });
+    const featuredMovieTrailer = await tmdb.get(
+      `/3/movie/${movies.data.results[0].id}/videos`,
+      {
+        params: {
+          api_key: "1155f6c239cb4332df695fcf245eaffd"
+        }
+      }
+    );
     this.setState({
-      movies: response.data.results,
-      featuredMovie: response.data.results[0]
+      dataLoaded: true,
+      movies: movies.data.results,
+      featuredMovie: movies.data.results[0],
+      featuredMovieTrailer: featuredMovieTrailer.data.results[0]
     });
   };
 
+  // When App component mounts, get movie data from tmdb using "discover" action and store it in state
+  componentDidMount() {
+    this.getMovies("discover");
+  }
+
+  // When SearchBar component is submitted, get movie data from tmdb using "search" action and input query and store it in state
+  onFormSubmit = (input) => {
+    this.getMovies("search", input);
+  };
+
   render() {
-    return (
-      <div>
-        <SearchBar onFormSubmit={this.onFormSubmit} />
-        <FeaturedMovie featuredMovie={this.state.featuredMovie} />
-        <MoviesList movies={this.state.movies} />
-      </div>
-    );
+    if (this.state.dataLoaded) {
+      return (
+        <div>
+          <SearchBar onFormSubmit={this.onFormSubmit} />
+          <FeaturedMovie
+            featuredMovie={this.state.featuredMovie}
+            featuredMovieTrailer={this.state.featuredMovieTrailer}
+          />
+          <MoviesList movies={this.state.movies} />
+          <Footer />
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
