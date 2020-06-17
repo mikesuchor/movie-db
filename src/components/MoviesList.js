dc7e504bf0a020c25f7ced58e7c9d3c97f76361e
mikesuchor <mikesuchor@gmail.com>
2020-06-17 09:54:45 -0400

    Added flickity carousel to MoviesList component

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,23 +1,35 @@
 import React from "react";
+import Flickity from "flickity";
 import MovieItem from "./MovieItem";
 import "./css/MoviesList.css";
 
-const MoviesList = ({ movies, onClickMovieItem }) => {
-  const renderedList = movies.map((movie) => {
+class MoviesList extends React.Component {
+  componentDidMount() {
+    new Flickity(".movies-list", {
+      cellAlign: "left",
+      contain: true
+    });
+  }
+
+  render() {
+    const { movies, onClickMovieItem } = this.props;
+
+    const renderedList = movies.map((movie) => {
+      return (
+        <MovieItem
+          key={movie.id}
+          movie={movie}
+          onClickMovieItem={onClickMovieItem}
+        />
+      );
+    });
     return (
-      <MovieItem
-        key={movie.id}
-        movie={movie}
-        onClickMovieItem={onClickMovieItem}
-      />
+      <div className="movies-section">
+        <h2>TRENDING MOVIES</h2>
+        <div className="movies-list">{renderedList}</div>
+      </div>
     );
-  });
-  return (
-    <div className="movies-section">
-      <h2>TRENDING MOVIES</h2>
-      <div className="movies-list">{renderedList}</div>
-    </div>
-  );
-};
+  }
+}
 
 export default MoviesList;
