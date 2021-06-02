61881d1fccf582f4c5f131f321e83a4d13b708ef
mikesuchor <mikesuchor@gmail.com>
2021-06-02 18:24:53 -0400

    Added conditional rendering to favorites list for empty list

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -3,25 +3,26 @@ import FavoriteItem from './FavoriteItem';
 import './css/FavoritesList.css';
 
 const FavoritesList = ({ favorites, onRemoveFavorite, onClickMovieItem }) => {
-  const renderedList = favorites
-    ? favorites.map((movie) => {
-        return (
-          <FavoriteItem
-            key={movie.id}
-            movie={movie}
-            onRemoveFavorite={onRemoveFavorite}
-            onClickMovieItem={onClickMovieItem}
-          />
-        );
-      })
-    : null;
+  console.log(favorites);
+  if (favorites.length) {
+    const renderedList = favorites.map((movie) => {
+      return (
+        <FavoriteItem
+          key={movie.id}
+          movie={movie}
+          onRemoveFavorite={onRemoveFavorite}
+          onClickMovieItem={onClickMovieItem}
+        />
+      );
+    });
 
-  return (
-    <div className="favorites-section">
-      <h2>FAVORITES LIST</h2>
-      <div className="favorites-list">{renderedList}</div>
-    </div>
-  );
+    return (
+      <div className="favorites-section">
+        <h2>FAVORITES LIST</h2>
+        <div className="favorites-list">{renderedList}</div>
+      </div>
+    );
+  } else return null;
 };
 
 export default FavoritesList;
