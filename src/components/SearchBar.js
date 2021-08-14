65ebe644db27529229d79ad0d755893bcf865974
mikesuchor <mikesuchor@gmail.com>
2021-08-14 12:19:42 -0400

    Change to class component and add hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import './css/SearchBar.css';
 
-class SearchBar extends React.Component {
-  state = { input: '' };
+const SearchBar = ({ onSearchSubmit }) => {
+  const [input, handleInput] = React.useState('');
 
-  onSearchSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSearchSubmit(this.state.input);
+    onSearchSubmit(input);
   };
 
-  onChangeInput = (event) => {
-    this.setState({ input: event.target.value });
+  const onChangeInput = (event) => {
+    handleInput(event.target.value);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui search">
-        <form className="search-form ui icon input" onSubmit={this.onSearchSubmit}>
-          <i className="search icon"></i>
-          <input
-            className="search-field"
-            type="text"
-            name="search"
-            placeholder="Search..."
-            value={this.state.input}
-            onChange={this.onChangeInput}
-          />
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui search">
+      <form className="search-form ui icon input" onSubmit={handleSubmit}>
+        <i className="search icon"></i>
+        <input
+          className="search-field"
+          type="text"
+          name="search"
+          placeholder="Search..."
+          value={input}
+          onChange={onChangeInput}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
